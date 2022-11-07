fd173ba161e8d0c0ade7310e5cd9434bd03c430e
Douglas Santana Costa <douglas.santana1777@gmail.com>
2022-11-07 17:24:57 -0300

    implementando a ordem decrescente

--- sortArrayString.js
+++ sortArrayString.js
@@ -1,5 +1,11 @@
 const pessoas = ["Joao", "ana", "carlos", "beatriz"];
+//ordenação crescente
 pessoas.sort((a, b) => {
   return a.localeCompare(b);
+});
+console.log(pessoas);
+//ordenação decrescente
+pessoas.sort((a, b) => {
+  return b.localeCompare(a);
 });
 console.log(pessoas);
